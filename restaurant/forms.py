0f8022981167e5377a36f8b7d4d516e497a621eb
Abdelrhiim <eng_abdelrhiim@yahoo.com>
2023-02-06 14:04:40 +0200

    my app commit

---
+++ restaurant/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+from .models import Booking
+
+#x Code added for loading form data on the Booking page
+class BookingForm(ModelForm):
+    class Meta:
+        model = Booking
+        fields = "__all__"
